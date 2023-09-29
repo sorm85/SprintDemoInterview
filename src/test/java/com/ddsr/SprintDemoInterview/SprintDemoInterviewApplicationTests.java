14ea3af944d4e335889e9324ae92c15e8c0eecc1
ddsr <dam2600@gmail.com>
2023-09-28 20:17:13 -0700

    adding prokect base

---
+++ src/test/java/com/ddsr/SprintDemoInterview/SprintDemoInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddsr.SprintDemoInterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintDemoInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
